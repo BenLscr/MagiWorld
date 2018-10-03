4c21301cd6c5b1320905d96d7d9290755d620e38
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2018-10-03 16:02:48 +0200

    Add choixDuJoueur1 in Joueur 1

--- src/com/ben/DéroulementDuJeux/Joueur1.java
+++ src/com/ben/DéroulementDuJeux/Joueur1.java
@@ -4,8 +4,41 @@ public class Joueur1 {
 
     public static void displayAvailablePersonnage() {
         System.out.println("Création du personnage du joueur 1");
-        System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
+        System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 Mage)");
     }
 
+    public String choixDuJoueur1() {
+        int nbPersonnage = askPersonnage();
+        int nbNiveau = -1;
+        int nbForce = -1;
+        int nbAgilite = -1;
+        int nbIntelligence = -1;
+        switch (nbPersonnage) {
+            case 1 :
+                nbNiveau = askNiveau();
+                nbForce = askForce();
+                nbAgilite = askAgilite();
+                nbIntelligence = askIntelligence();
+                break;
+            case 2 :
+                nbNiveau = askNiveau();
+                nbForce = askForce();
+                nbAgilite = askAgilite();
+                nbIntelligence = askIntelligence();
+                break;
+            case 3 :
+                nbNiveau = askNiveau();
+                nbForce = askForce();
+                nbAgilite = askAgilite();
+                nbIntelligence = askIntelligence();
+                break;
+        }
+        return ecrireLapetitephrasequiestdansleterminal;
+    }
+
+    public int askPersonnage() {
+        String[] personnages = {"Guerrier", "Rôdeur", "Mage"};
+        return nbPersonnage;
+    }
 
 }
